e0a89a51fc8eae138aedc9095c7ed4351157f4b6
Douglas Winter <132670815+DGzzzzz@users.noreply.github.com>
2024-02-17 22:19:16 -0300

    ajuste scripts

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const uglify = require('gulp-uglify');
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js/main.js'));
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
 }
 
 function styles() {
-    return gulp.src('./src/styles/*scss')
-        .pipe(sass({ outputStyle: 'compressed'}))
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
@@ -22,7 +22,8 @@ function images() {
 }
 
 exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function() {
-    gulp.watch('./src/styles/*scss', gulp.parallel(styles));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
